0c458d2685cebb847ed80beb9d007f75991fabb8
unknown <ceedriclazaro010205@gmail.com>
2024-10-08 07:10:29 -0800

    Initial commit

---
+++ taskCalendar.js
@@ -0,0 +1,21 @@
+function taskCalendar() {
+    let day = prompt("Enter your age:");
+
+switch (day) {
+     case "Monday":
+        alert("Attend in class");
+        break;
+    case "Tuesday":
+        alert("Clean the house");
+        break;
+    case "Wednesday":
+        alert("Do your laundry");
+        break;
+    case "Thursday":
+        alert("Do your homeworks");
+        break;
+    case "Friday":
+        alert("Do groceries");
+        break;
+    }
+}
